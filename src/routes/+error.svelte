adfe856181e5c8527403ee4f33c87453c7b91563
brequet <requet.baptiste@gmail.com>
2024-10-27 11:38:45 +0100

    github action

---
+++ src/routes/+error.svelte
@@ -0,0 +1,4 @@
+<h1 class="text-2xl font-bold">Something went wrong</h1>
+
+<p class="text-lg">This page could not be found.</p>
+<a href="/" class="text-blue-500 underline">Go back home</a>
--- svelte.config.js
+++ svelte.config.js
@@ -11,9 +11,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: '404.html'
+		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : '/dev-tools'
 		}
 	}
 };
