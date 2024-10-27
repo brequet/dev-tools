ca0bca40ad1c864183252b7c91415e99ee8b33fb
brequet <requet.baptiste@gmail.com>
2024-10-27 11:42:44 +0100

    github action

--- svelte.config.js
+++ svelte.config.js
@@ -11,11 +11,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: '404.html'
-		}),
+		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/dev-tools'
+			base: process.env.NODE_ENV === 'development' ? '' : '/dev-tools'
+		},
+		trailingSlash: 'always', // Ensures trailing slash consistency
+
+		prerender: {
+			enabled: true,
+			entries: ['/'] // Add other paths if needed
 		}
 	}
 };
