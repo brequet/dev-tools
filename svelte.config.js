e15dfecb34f5e7ecfca905ca851cf7dc53732863
brequet <requet.baptiste@gmail.com>
2024-10-27 12:08:21 +0100

    github page 404 page

--- svelte.config.js
+++ svelte.config.js
@@ -11,13 +11,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: '404.html',
+		}),
 		paths: {
 			base: '/dev-tools'
 		},
-		prerender: {
-			entries: ['*'] // This generates a 404.html for unknown paths
-		}
 	}
 };
 
